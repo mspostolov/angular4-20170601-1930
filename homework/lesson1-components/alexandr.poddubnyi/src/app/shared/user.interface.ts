0d9a3532ae60ec827c1b5eba274652080079f832
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2017-06-06 22:18:17 +0300

    done homework

---
+++ homework/lesson1-components/alexandr.poddubnyi/src/app/shared/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  fullName: string;
+  avatarUrl: string;
+  birthdate: string;
+  gender: string;
+  address: string;
+  email: string;
+  highlighted?: boolean;
+}
