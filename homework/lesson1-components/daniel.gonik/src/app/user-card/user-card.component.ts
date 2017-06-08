8dc3463134902de65721afbd8bc3e5be55eaeace
Gonik Daniel <gonik.daniel88@gmail.com>
2017-06-08 03:40:42 +0300

    Logic and animation

--- homework/lesson1-components/daniel.gonik/src/app/user-card/user-card.component.ts
+++ homework/lesson1-components/daniel.gonik/src/app/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'brr-user-card',
@@ -8,10 +8,30 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UserCardComponent implements OnInit {
 
   @Input() user;
+  @ViewChild('userCard') userCard: ElementRef;
+  public isOpen: boolean = false;
+  public showSpinner: boolean = false;
 
-  constructor() { }
+  constructor(private _renderer: Renderer2) {}
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // console.log(this.userCard.nativeElement);
+  }
+
+  get active() {
+    return this.isOpen
+      ? 'mc-active'
+      : '';
+  }
+
+  toggleInfo() {
+    this.isOpen = !this.isOpen;
+    this.showSpinner = true;
+
+    setTimeout(() => this.showSpinner = false, 800);
   }
 
 }
