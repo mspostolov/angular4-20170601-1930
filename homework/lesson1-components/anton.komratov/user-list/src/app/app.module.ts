a05ebb8c2620a3d63253a027d439133300d144e0
Anton Komratov <a.komratov@yandex.ru>
2017-06-08 17:48:50 +0300

    Hometask01 completed

--- homework/lesson1-components/anton.komratov/user-list/src/app/app.module.ts
+++ homework/lesson1-components/anton.komratov/user-list/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule
