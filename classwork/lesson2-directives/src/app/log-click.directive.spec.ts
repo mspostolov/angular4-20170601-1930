218df90387cc41c3a9d50b9c5c821dc7e1b5934a
Aleksandr Lupa <aleksandr.lupa@p2h.com>
2017-06-11 22:51:30 +0300

    clear classwork

--- classwork/lesson2-directives/src/app/log-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LogClickDirective } from './log-click.directive';
-
-describe('LogClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LogClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- classwork/lesson2-directives/src/app/myformat.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyformatPipe } from './myformat.pipe';
-
-describe('MyformatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyformatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
