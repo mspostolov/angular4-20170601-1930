36e8880ffc6f183422100e0dc71b37cb091074ca
alexandr.poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2017-06-11 13:32:29 +0300

    init project

---
+++ homework/alexandr.poddubnyi/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { HomeworkPage } from './app.po';
+
+describe('homework App', () => {
+  let page: HomeworkPage;
+
+  beforeEach(() => {
+    page = new HomeworkPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
