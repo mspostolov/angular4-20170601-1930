2d7b0e45ff032ec06305350ea194743c83292a3b
Anton Komratov <a.komratov@yandex.ru>
2017-06-13 17:18:58 +0300

    Hometask03 - part 1 (user list) completed

--- homework/anton.komratov/user-list/src/app/app.module.ts
+++ homework/anton.komratov/user-list/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCardComponent } from './user-list/user-card/user-card.component';
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
