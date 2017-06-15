abd933690ad1f44b7832acdf934e77f15ad25572
Stanislav <novitckas@gmail.com>
2017-06-15 07:41:34 +0300

    homework-3 currency layout.

--- homework/stanislav.novitckas/src/app/currency-converter/currency-converter.component.ts
+++ homework/stanislav.novitckas/src/app/currency-converter/currency-converter.component.ts
@@ -7,13 +7,18 @@ import {CurrencyExchangeRateService} from "../currency-exchange-rate.service";
   styleUrls: ['./currency-converter.component.css']
 })
 export class CurrencyConverterComponent implements OnInit {
+  public valuteList: [{}] = [{Name: 'Российский рубль', Value: 1}];
 
   constructor(public currencyExchangeRateService: CurrencyExchangeRateService) { }
 
   ngOnInit() {
     this.currencyExchangeRateService.getCurrencyExchangeRate()
       .subscribe((data) => {
-        console.log('currencyExchangeRateService', data);
+        for (let key in data.Valute) {
+          this.valuteList.push(data.Valute[key])
+        }
+        console.log('currencyExchangeRateService', data.Valute);
+        console.log('valuteList', this.valuteList);
       })
   }
 
