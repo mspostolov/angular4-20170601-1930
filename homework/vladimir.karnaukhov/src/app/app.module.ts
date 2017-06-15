2d69e5466f65089404b24f4172a7c87d93ca70f4
Vladimir Karnaukhov <gepard11vvk@gmail.com>
2017-06-15 18:06:50 +0300

    Add users list with HTTP interaction.

--- homework/vladimir.karnaukhov/src/app/app.module.ts
+++ homework/vladimir.karnaukhov/src/app/app.module.ts
@@ -1,23 +1,34 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './user-list/services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { MailListComponent } from './mail-list/mail-list.component';
-import { EmailsService } from './services/emails.service';
-import { ReceivingDateFormatPipe } from './pipes/receiving-date-format.pipe';
-import { MailItemComponent } from './mail-list/mail-item/mail-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCardComponent } from './user-list/user-card/user-card.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { UsersService } from './user-list/services/users-service.service';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MailListComponent,
-    ReceivingDateFormatPipe,
-    MailItemComponent
+    UserListComponent,
+    UserCardComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
-  providers: [EmailsService],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- homework/vladimir.karnaukhov/src/app/user-list/user-list.component.spec.ts
+++ homework/vladimir.karnaukhov/src/app/user-list/user-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MailItemComponent } from './mail-item.component';
+import { UserListComponent } from './user-list.component';
 
-describe('MailItemComponent', () => {
-  let component: MailItemComponent;
-  let fixture: ComponentFixture<MailItemComponent>;
+describe('UserListComponent', () => {
+  let component: UserListComponent;
+  let fixture: ComponentFixture<UserListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MailItemComponent ]
+      declarations: [ UserListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MailItemComponent);
+    fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
