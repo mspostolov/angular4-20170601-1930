07ba6fac68f32b252d1ae88a1be108d1164ed8dc
alexandr poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2017-06-16 16:07:25 +0300

    add Observable.fromEvent behavior

--- homework/alexandr.poddubnyi/src/app/users-currency/currency-converter/currency-converter.component.ts
+++ homework/alexandr.poddubnyi/src/app/users-currency/currency-converter/currency-converter.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, ElementRef, OnInit,
+  ViewChild
+} from '@angular/core';
 import { CurrencyService } from 'app/users-currency/currency.service';
 import { ICurrency } from 'app/users-currency/shared/currency';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'currency-converter',
   templateUrl: './currency-converter.component.html',
   styleUrls: ['./currency-converter.component.css'],
-  providers: [CurrencyService]
+  providers: [CurrencyService],
 })
 export class CurrencyConverterComponent implements OnInit {
   currencies: ICurrency[];
   currency = 0;
   amount = 0;
   convertedValue = 0;
+
+  @ViewChild('amountElem') amountElem: ElementRef;
+  @ViewChild('convertedElem') convertedElem: ElementRef;
 
   constructor(private currencyService: CurrencyService) { }
 
@@ -22,6 +32,8 @@ export class CurrencyConverterComponent implements OnInit {
         this.currencies = currencies;
         this.currency = +this.currencies[0].sale;
       });
+
+    this.addBindings();
   }
 
   setConvertedValue() {
@@ -30,5 +42,23 @@ export class CurrencyConverterComponent implements OnInit {
 
   setAmountValue() {
     this.amount = +this.convertedValue / +this.currency;
+  }
+
+  private addBindings() {
+    Observable.fromEvent<KeyboardEvent>(this.amountElem.nativeElement, 'input')
+      .map(event => (event.target as HTMLInputElement).value)
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        this.setConvertedValue();
+      });
+
+    Observable.fromEvent<KeyboardEvent>(this.convertedElem.nativeElement, 'input')
+      .map(event => (event.target as HTMLInputElement).value)
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        this.setAmountValue();
+      });
   }
 }
