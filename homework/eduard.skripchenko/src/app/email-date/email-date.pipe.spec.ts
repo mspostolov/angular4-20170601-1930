16e0dda70278698cf75c04ff0f4f8b4d1e7f5dfe
e-watson <eduard.skripchenko@icloud.com>
2017-06-19 08:51:25 +0300

    Observables

--- homework/eduard.skripchenko/src/app/email-date/email-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmailDatePipe } from './email-date.pipe';
-
-describe('EmailDatePipe', () => {
-    it('create an instance', () => {
-        const pipe = new EmailDatePipe();
-        expect(pipe).toBeTruthy();
-    });
-});
--- homework/eduard.skripchenko/src/app/search/search.component.spec.ts
+++ homework/eduard.skripchenko/src/app/search/search.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserCardComponent } from './user-card.component';
+import { SearchComponent } from './search.component';
 
-describe('UserCardComponent', () => {
-  let component: UserCardComponent;
-  let fixture: ComponentFixture<UserCardComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserCardComponent ]
+      declarations: [ SearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserCardComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
