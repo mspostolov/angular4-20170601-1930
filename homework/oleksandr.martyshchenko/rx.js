efc23ce197c83baafa819a9fa61bdf0f5693bb6d
Ketler <ketler13@yandex.ru>
2017-06-19 14:58:31 +0300

    observable sandbox

--- homework/oleksandr.martyshchenko/rx.js
+++ homework/oleksandr.martyshchenko/rx.js
@@ -1,4 +1,5 @@
 const { Observable } = require('rxjs/Observable');
+const { Subject } = require('rxjs/Subject');
 const axios = require('axios');
 
 require("rxjs/add/observable/interval");
@@ -18,8 +19,7 @@ const source = Observable
     Observable.fromPromise(axios.get(URL + Math.floor(Math.random()*300)))
   ))
   .map(res => res.data.filter(user => user.id % 3 === 0 && user.login.length > 10))
-  //.map(users => users.filter(user => user.id % 3 === 0 && user.login.length > 10))
-  .take(1);
+  .take(Math.ceil(Math.random() * 25));
 
 source.subscribe(
   users => console.log(users),
@@ -29,29 +29,58 @@ source.subscribe(
 
 //------------creating observable 2
 
-// const source2 = Observable.create(observer => {
-//   const timer = setInterval(() => {
-//     const factor = Math.random();
-//     if (factor < 0.8) {
-//       observer.next('next value');
-//     }
-//     if (factor >= 0.8 && factor < 0.9) {
-//       throw new Error('Sad but true..');
-//     }
-//     if (factor >= 0.9) {
-//       observer.complete();
-//     }
-//   }, 2000);
-//
-//   return function unsubscribe() {
-//     clearInterval(timer);
-//   }
-// });
-//
-// const subscriber = source2.subscribe(
-//   result => console.log(result),
-//   error => console.log(error),
-//   () => console.log('completed')
-// );
+const source2 = Observable.create(observer => {
+  const timer = setInterval(() => {
+    const factor = Math.random();
+    if (factor < 0.8) {
+      observer.next('next value');
+    }
+    if (factor >= 0.8 && factor < 0.9) {
+      throw new Error('Sad but true..');
+    }
+    if (factor >= 0.9) {
+      observer.complete();
+    }
+  }, 2000);
 
-//subscriber.unsubscribe();
+  return function unsubscribe() {
+    clearInterval(timer);
+  }
+});
+
+const subscriber = source2.subscribe(
+  result => console.log(result),
+  error => console.log(error),
+  () => console.log('completed')
+);
+
+subscriber.unsubscribe();
+
+// const subject = new Subject();
+//
+// const subscriber1 = subject.subscribe(
+//   x => console.log(x),
+//   err => console.log(err),
+//   () => console.log(competed)
+// );
+//
+// const subscriber2 = subject.subscribe(
+//   x => console.log(x),
+//   err => console.log(err),
+//   () => console.log(competed)
+// );
+//
+// const timer1 = setInterval(() => {
+//   subscriber1.next(1);
+// }, 1000);
+//
+// const timer2 = setInterval(() => {
+//   subscriber2.next(2);
+// }, 500);
+//
+// setTimeout(() => {
+//   subscriber1.complete();
+//   subscriber2.complete();
+//   clearInterval(timer1);
+//   clearInterval(timer2);
+// }, 5000)
