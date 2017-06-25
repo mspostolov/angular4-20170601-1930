2bc3a360a42bccba7769dc150099e4d0b28205ba
AterStrix <artem.aterstrix@gmail.com>
2017-06-25 21:42:48 +0300

    Home work 5

--- homework/artem.gnatenko/src/app/app.module.ts
+++ homework/artem.gnatenko/src/app/app.module.ts
@@ -3,44 +3,45 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { JsonpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MailBoxComponent } from './mail-box/mail-box.component';
-import { TimeElapsedPipe } from './pipes/time-elapsed/time-elapsed.pipe';
-import { MailComponent } from './mail-box/mail/mail.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserCardComponent } from './user-list/user-card/user-card.component';
-import { CountryfromISOPipe } from './pipes/country-from-iso/countryfrom-iso.pipe';
-import { UsersService } from './user-list/users.service';
 import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 import { CurrencyConverterService } from './currency-converter/currency-converter.service';
 import { SearchComponent } from './search/search.component';
 import { WikiSearchComponent } from './search/wiki-search/wiki-search.component';
 import { SearchService } from './search/search.service';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login/login.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MailBoxComponent,
-    TimeElapsedPipe,
-    MailComponent,
-    UserListComponent,
-    UserCardComponent,
-    CountryfromISOPipe,
     CurrencyConverterComponent,
     SearchComponent,
-    WikiSearchComponent
+    WikiSearchComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { 
+        path: 'mails-and-users', 
+        loadChildren: 'app/mails-and-users/mails-and-users.module#MailsAndUsersModule', 
+        canActivate: [AuthGuard]
+      }
+    ])
   ],
   providers: [
-    UsersService,
     CurrencyConverterService,
-    SearchService
+    SearchService,
+    LoginService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
