5ce27d3704c3698b43355e2c52e76857da2e9c21
Stanislav <novitckas@gmail.com>
2017-06-25 13:27:31 +0300

    homework-5 init

--- homework/stanislav.novitckas/src/app/app.module.ts
+++ homework/stanislav.novitckas/src/app/app.module.ts
@@ -14,6 +14,17 @@ import {CurrencyExchangeRateService} from "./currency-exchange-rate.service";
 import {CurrencyConverterService} from "./currency-converter.service";
 import { SearchComponent } from './wiki-search/search/search.component';
 import { WikiSearchComponent } from './wiki-search/wiki-search.component';
+import { LoginComponent } from './login/login.component';
+import {MdInputModule} from "@angular/material";
+import {Route, RouterModule} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+const routes: Route[] = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'mail-box', component: MailboxComponent},
+  {path: 'user-list', component: UserListComponent},
+]
 
 @NgModule({
   declarations: [
@@ -25,12 +36,22 @@ import { WikiSearchComponent } from './wiki-search/wiki-search.component';
     UserCardComponent,
     CurrencyConverterComponent,
     SearchComponent,
-    WikiSearchComponent
+    WikiSearchComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule, HttpModule, JsonpModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    JsonpModule,
+    MdInputModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [UserServiceService, CurrencyExchangeRateService, CurrencyConverterService],
+  providers: [
+    UserServiceService,
+    CurrencyExchangeRateService,
+    CurrencyConverterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
