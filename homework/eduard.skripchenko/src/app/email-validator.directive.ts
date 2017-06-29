cbae68345002924a631ca8fc6fa6b879c5811fb7
ESkripchenko <eduard.skripchenko@gmail.com>
2017-06-29 13:31:31 +0300

    Intermediate commit - III

--- homework/eduard.skripchenko/src/app/email-validator.directive.ts
+++ homework/eduard.skripchenko/src/app/email-validator.directive.ts
@@ -1,9 +1,32 @@
 import { Directive } from '@angular/core';
-import { NG_VALIDATORS, FormControl } from '@angular/forms';
+import { NG_VALIDATORS, FormControl, NG_ASYNC_VALIDATORS } from '@angular/forms';
 
-function emailValidator(control: FormControl): boolean {
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/first';
+
+import { DataService } from 'app/services/data.service';
+
+function emailValidator(control: FormControl) {
     const regExp = new RegExp(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i);
-    return regExp.test(control.value);
+    const isValid = regExp.test(control.value);
+
+    return isValid ? null : { validEmail: false };
+}
+
+function emailUniqueValidator(control: FormControl, dataService: DataService) {
+    const email = control.value;
+    console.log(dataService);
+
+    return new Observable(observer => {
+        if (email === 'jh.watson@gmail.co.uk') {
+            observer.next({ validEmail: false });
+        } else {
+            observer.next(null);
+        }
+
+    }).debounceTime(500).distinctUntilChanged().first();
 }
 
 @Directive({
@@ -12,8 +35,15 @@ function emailValidator(control: FormControl): boolean {
         {
             provide: NG_VALIDATORS,
             useValue: emailValidator,
+            deps: [ DataService ],
             multi: true
-        }
+        },
+        {
+            provide: NG_ASYNC_VALIDATORS,
+            useValue: emailUniqueValidator,
+            multi: true
+        },
+        DataService
     ]
 })
 export class EmailValidatorDirective {
