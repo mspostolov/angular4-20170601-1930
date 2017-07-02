3409e4cf550ebf901f9f0a140693c0953af90067
e-watson <eduard.skripchenko@icloud.com>
2017-07-02 22:16:42 +0300

    homework 7

--- homework/eduard.skripchenko/src/app/app.module.ts
+++ homework/eduard.skripchenko/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
-import { EmailValidatorDirective } from './email-validator.directive';
 
 const routes: Routes = [
     { path: '', redirectTo: '/users', pathMatch: 'full' },
@@ -18,13 +17,12 @@ const routes: Routes = [
     declarations: [
         AppComponent,
         UsersComponent,
-        UserComponent,
-        EmailValidatorDirective
+        UserComponent
     ],
     imports: [
         BrowserModule,
         RouterModule.forRoot(routes),
-        FormsModule
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
