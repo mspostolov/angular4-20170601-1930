54d84976094027f2a8e8059fe0483a3abc769dbb
dragozin <forspam_05@bk.ru>
2017-07-02 22:32:00 +0300

    reactive forms

--- homework/dmitry.ragozin/src/app/email-validator.directive.ts
+++ homework/dmitry.ragozin/src/app/email-validator.directive.ts
@@ -1,16 +1,9 @@
 import { Directive } from '@angular/core';
 import {NG_VALIDATORS, AbstractControl} from '@angular/forms';
 
-function emailValidator(control) {
-  if (!/[0-9a-z]+@[0-9a-z]+\.[a-z]{2,5}/i.test(control.value)) {
-    return {'emailValidator': control.value}
-  } else {
-    return null;
-  };
-}
 
 @Directive({
-  selector: '[emailValidator][ngModel]',
+  selector: '[emailValidatorAsync]',
   providers: [{
       provide: NG_VALIDATORS,
       //useValue: emailValidator,
@@ -22,7 +15,9 @@ export class EmailValidatorDirective {
 
   constructor() { }
   validate(control: AbstractControl): {[key: string]: any} {
-    return emailValidator(control);
+    return null;
   }
+
+
 
 }
