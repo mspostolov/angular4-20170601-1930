05292cb6a3533ff77782adb0e1edcae6e62ea036
Stanislav <novitckas@gmail.com>
2017-07-08 14:38:42 +0300

    commits remove

--- homework/stanislav.novitckas/src/app/user-list/children-test/children-test.component.ts
+++ homework/stanislav.novitckas/src/app/user-list/children-test/children-test.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {MdDialog} from "@angular/material";
-import {PopupUserDetailComponent} from "../popup-user-detail/popup-user-detail.component";
 import {UserCardDetailComponent} from "../user-card-detail/user-card-detail.component";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {UserServiceService} from "../../user-service.service";
@@ -19,13 +18,9 @@ export class ChildrenTestComponent implements OnInit {
   ) { }
 //Здесь закончил!!!
   ngOnInit() {
-    this.route.params
-      .subscribe((data) => console.log('asdasdas', data))
-    // console.log('snapShot', this.snapShot);
     this.route.paramMap
       .subscribe((params: ParamMap) => {
         this.urlParam = +params.get('id');
-        console.log('urlParamChildrenTest', this.urlParam, params)
         this.dialog.open(UserCardDetailComponent, {
           data: this.urlParam
         })
